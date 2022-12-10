2c0655ceafa5b32b6d48de596a78a754c513e89d
EsUnoAlpha <1687462@gmail.com>
2022-12-10 13:13:16 +0300

    Methods added

---  Encapsulation/Task2.py
+++  Encapsulation/Task2.py
@@ -6,6 +6,7 @@
 from random import randint
 from time import sleep
 
+
 class Hero():
     def __init__(self, name, health, armor, power, weapon, rang):
         self.name = name
@@ -14,17 +15,18 @@ class Hero():
         self.power = power
         self.weapon = weapon
         self.__rang = rang
+
     def print_info(self):
         sleep(1)
-        print('Поприветствуйте героя:', self.name )
+        print('Поприветствуйте героя:', self.name)
         sleep(1)
-        print('Уровень здоровья:', self.health )
+        print('Уровень здоровья:', self.health)
         sleep(1)
-        print('Броня:', self.armor )
+        print('Броня:', self.armor)
         sleep(1)
-        print('Сила удара:', self.power )
+        print('Сила удара:', self.power)
         sleep(1)
-        print('Оружие:', self.weapon , '\n')
+        print('Оружие:', self.weapon, '\n')
 
     def getrang(self):
         return self.__rang
@@ -37,34 +39,38 @@ class Hero():
 
     def strike_hero(self, rascal):
         print(
-            'Удар! ' + self.name + ' атакует ' + rascal.name + ' с силой ' + str(self.power) + ', используя ' + self.weapon +'\n'
+            'Удар! ' + self.name + ' атакует ' + rascal.name + ' с силой ' + str(self.power) + ', используя '
+            + self.weapon + '\n'
         )
-        rascal.armor -= knight.power
+        rascal.armor -= self.power
         if rascal.armor < 0:
             rascal.health += rascal.armor
             rascal.armor = 0
         sleep(2)
-        print (
-            "Класс брони " + rascal.name + ' упал до ' + str(rascal.armor) + ', а количество здоровья уменьшилось до ' + str(rascal.health) + '\n'
+        print(
+            "Класс брони " + rascal.name + ' упал до ' + str(rascal.armor) + ', а количество здоровья уменьшилось до '
+            + str(rascal.health) + '\n'
         )
 
     def strike_enemy(self, knight):
         print(
-            'Удар! ' + self.name + ' атакует ' + knight.name + ' с силой ' + str(self.power) + ', используя ' + self.weapon +'\n'
+            'Удар! ' + self.name + ' атакует ' + knight.name + ' с силой ' + str(self.power) + ', используя '
+            + self.weapon + '\n'
         )
-        knight.armor -= rascal.power
+        knight.armor -= self.power
         if knight.armor < 0:
             knight.health += knight.armor
             knight.armor = 0
         sleep(2)
-        print (
-            "Класс брони", knight.name, 'упал до ' + str(knight.armor) + ', а количество здоровья уменшьшилось до ' + str(knight.health) + '\n'
+        print(
+            "Класс брони", knight.name, 'упал до ' + str(knight.armor) + ', а количество здоровья уменшьшилось до '
+            + str(knight.health) + '\n'
         )
 
 
-knight = Hero('Ричард', randint(100, 500), randint(5, 50), randint(5, 200), 'Меч', 'Герой')
+knight = Hero('Ричард', randint(100, 500), randint(20, 100), randint(5, 200), 'Меч', 'Герой')
 knight.print_info()
-rascal = Hero('Хелен', randint(100, 500), randint(5, 50), randint(5, 200), 'Лук', 'Монстр')
+rascal = Hero('Хелен', randint(100, 500), randint(20, 100), randint(5, 200), 'Лук', 'Монстр')
 rascal.print_info()
 while True:
     if knight.health > 0:
@@ -87,4 +93,6 @@ while True:
         print(knight.getrang())
         rascal.delrang()
         break
+
+
 
