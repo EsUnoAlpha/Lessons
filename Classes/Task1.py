f7be8a3370e01af9fd70ca59939cd4d9efe78758
EsUnoAlpha <1687462@gmail.com>
2022-12-13 13:41:46 +0300

    compositions added

--- Classes/Task1.py
+++ Classes/Task1.py
@@ -1,12 +1,14 @@
 from time import sleep
 
-class Hero():
+
+class Hero:
     def __init__(self, name, health, armor, power, weapon):
         self.name = name
         self.health = health
         self.armor = armor
         self.power = power
         self.weapon = weapon
+
     def print_info(self):
         sleep(1)
         print('Поприветствуйте героя:', self.name)
@@ -17,36 +19,40 @@ class Hero():
         sleep(1)
         print('Сила удара:', self.power)
         sleep(1)
-        print('Оружие:', self.weapon)
+        print('Оружие:', self.weapon, '\n')
 
     def strike_hero(self, rascal):
         print(
-            'Удар! ' + self.name + ' атакует ' + rascal.name + ' с силой ' + str(self.power) + ', используя ' + self.weapon +'\n'
+            'Удар! ' + self.name + ' атакует ' + rascal.name + ' с силой ' + str(self.power) + ', используя '
+            + self.weapon + '\n'
         )
-        rascal.armor -= knight.power
+        rascal.armor -= self.power
         if rascal.armor < 0:
             rascal.health += rascal.armor
             rascal.armor = 0
         sleep(2)
-        print (
-            "Класс брони " + rascal.name + ' упал до ' + str(rascal.armor) + ', а количество здоровья уменьшилось до ' + str(rascal.health) + '\n'
+        print(
+            "Класс брони " + rascal.name + ' упал до ' + str(rascal.armor) + ', а количество здоровья уменьшилось до '
+            + str(rascal.health) + '\n'
         )
 
     def strike_enemy(self, knight):
         print(
-            'Удар! ' + self.name + ' атакует ' + knight.name + ' с силой ' + str(self.power) + ', используя ' + self.weapon +'\n'
+            'Удар! ' + self.name + ' атакует ' + knight.name + ' с силой ' + str(self.power) + ', используя '
+            + self.weapon + '\n'
         )
-        knight.armor -= rascal.power
+        knight.armor -= self.power
         if knight.armor < 0:
             knight.health += knight.armor
             knight.armor = 0
         sleep(2)
-        print (
-            "Класс брони", knight.name, 'упал до ' + str(knight.armor) + ', а количество здоровья уменшьшилось до ' + str(knight.health) + '\n'
+        print(
+            "Класс брони", knight.name, 'упал до ' + str(knight.armor) + ', а количество здоровья уменшьшилось до '
+            + str(knight.health) + '\n'
         )
 
 
-knight = Hero('Ричард', 50, 25, 20, 'Меч')
+knight = Hero('Ричард', 50, 20, 25, 'Меч')
 knight.print_info()
 rascal = Hero('Хелен', 20, 5, 5, 'Лук')
 rascal.print_info()
@@ -55,7 +61,7 @@ while True:
         sleep(2)
         knight.strike_hero(rascal)
     else:
-        sleep(22)
+        sleep(2)
         print('Монстр победил!')
         break
     if rascal.health > 0:
@@ -65,5 +71,3 @@ while True:
         sleep(2)
         print('Рыцарь победил!')
         break
-
-
